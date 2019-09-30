0367685b346330468918175ad87f3e4d8748e389
Pierre-Alexandre <contact@b3dgs.com>
2019-09-30 21:58:39 +0200

    License header updated.

--- sample-android/app/src/main/java/com/b3dgs/sample/android/ActivitySample.java
+++ sample-android/app/src/main/java/com/b3dgs/sample/android/ActivitySample.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 2013-2019 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
