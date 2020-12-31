93dc350118204f9025baa4204daf81147be09c30
DjThunder <contact@b3dgs.com>
2020-12-31 18:31:00 +0100

    Updated after https://github.com/b3dgs/lionengine/issues/681

--- sample-game/src/main/java/com/b3dgs/sample/Scene.java
+++ sample-game/src/main/java/com/b3dgs/sample/Scene.java
@@ -27,7 +27,7 @@ import com.b3dgs.lionengine.graphic.Text;
 /**
  * Game scene implementation.
  */
-public final class Scene extends SequenceGame
+public final class Scene extends SequenceGame<World>
 {
     private final Text text = Graphics.createText(20);
 
