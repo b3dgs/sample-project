2d3d14fedd84a760a59c904e1be19510452b9194
Pierre-Alexandre <contact@b3dgs.com>
2023-12-31 17:04:08 +0100

    Coverage updated. Log4j2 configuration added.

---
+++ sample-pc/src/test/java/com/b3dgs/sample/pc/AppSampleTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2013-2023 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.b3dgs.sample.pc;
+
+import static com.b3dgs.lionengine.UtilAssert.assertPrivateConstructor;
+
+import org.junit.jupiter.api.Test;
+
+import com.b3dgs.lionengine.Engine;
+import com.b3dgs.lionengine.UtilTests;
+
+/**
+ * Test correct {@link AppSample} loading.
+ */
+final class AppSampleTest
+{
+    /**
+     * Test the constructor.
+     */
+    @Test
+    void testConstructorPrivate()
+    {
+        assertPrivateConstructor(AppSample.class);
+    }
+
+    /**
+     * Test app.
+     */
+    @Test
+    void testApp()
+    {
+        AppSample.main(new String[] {});
+        UtilTests.pause(1000L);
+        Engine.terminate();
+    }
+}
